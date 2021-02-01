02e668d31e00abcaf9a6dd2453758f3354765e08
PhoenixNest <XenOmeGA@163.com>
2021-02-01 22:30:18 +0800

    Site updated: 2021-02-01 22:30:15

--- js/obsidian.js
+++ js/obsidian.js
@@ -1332,10 +1332,10 @@ $(function () {
   utiliseBgColor();
   initialTyped();
   Obsidian.setCodeRowWithLang();
-  console.log(
-    '%c Github %c',
-    'background:#24272A; color:#73ddd7',
-    '',
-    'https://github.com/TriDiamond/hexo-theme-obsidian'
-  );
+  // console.log(
+  //   '%c Github %c',
+  //   'background:#24272A; color:#73ddd7',
+  //   '',
+  //   'https://github.com/TriDiamond/hexo-theme-obsidian'
+  // );
 });
